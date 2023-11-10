58d7eb47c1b174284cfed8a4aed235fc05a59c63
Tony Chan <tonychan8201@gmail.com>
2023-11-10 11:04:37 +0800

    fix: server default with text

--- app/database/model/conversation_history.py
+++ app/database/model/conversation_history.py
@@ -1,5 +1,5 @@
 from app.database.base import Base
-from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import Column, Integer, String, TIMESTAMP, text
 from sqlalchemy.dialects.postgresql import UUID
 
 class ConversationHistory(Base):
@@ -7,13 +7,13 @@ class ConversationHistory(Base):
     __tablename__ = 'conversation_history'
 
     id = Column(Integer, primary_key=True, index=True)
-    conversation_id = Column(UUID(as_uuid=True), server_default='gen_random_uuid()')
+    conversation_id = Column(UUID(as_uuid=True), server_default=text('gen_random_uuid()'))
     human_message = Column(String, nullable=False)
     ai_message = Column(String, nullable=False)
     existing_summary = Column(String)
-    created_at = Column(TIMESTAMP, server_default='now()')
+    created_at = Column(TIMESTAMP, server_default=text('now()'))
     created_by = Column(String(255))
-    updated_at = Column(TIMESTAMP, server_default='now()')
+    updated_at = Column(TIMESTAMP, server_default=text('now()'))
     updated_by = Column(String(255))
     deleted_at = Column(TIMESTAMP)
     deleted_by = Column(String(255))
