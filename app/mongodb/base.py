1fe70ce26644a0cd1b0eaccdebe9381a8e9ae455
Tony Chan <tonychan8201@gmail.com>
2023-11-22 13:45:37 +0800

    feat: use mongodb gridfs

--- app/mongodb/base.py
+++ app/mongodb/base.py
@@ -1,8 +1,9 @@
-import motor.motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorGridFSBucket, AsyncIOMotorClient
 import os
 import dns
 
-client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv('MONGO_DATABASE_URL'))
+client = AsyncIOMotorClient(os.getenv('MONGO_DATABASE_URL'))
 db = client.get_database("chatbot")
+# gfs = AsyncIOMotorGridFSBucket(db)
 user_document_collection = db.get_collection('user_document_collection')
 
--- app/mongodb/crud/document.py
+++ app/mongodb/crud/document.py
@@ -1,25 +1,35 @@
-from app.mongodb.base import db
+from app.mongodb.base import db, client
 from pymongo.results import InsertOneResult
 from app.mongodb.schema.document import DocumentCreate, DocumentSchema
 from pymongo.collection import Collection
 from typing import List
 from pymongo.cursor import Cursor
-from motor.motor_asyncio import AsyncIOMotorCursor
+from motor.motor_asyncio import AsyncIOMotorCursor, AsyncIOMotorGridFSBucket
 
 document_collection: Collection = db.get_collection('user_document_collection')
 
 async def create_document(doc: DocumentCreate) -> dict:
-    persisted: InsertOneResult = await document_collection.insert_one(doc.__dict__)
-    entity = document_collection.find_one({'_id': persisted.inserted_id})
-    return entity
+    gfs = AsyncIOMotorGridFSBucket(db)
+
+    persisted = await gfs.upload_from_stream(filename=doc.filename, 
+                                             source=doc.content, 
+                                             metadata={'mime_type': doc.mime_type, 'conversation_id': doc.conversation_id})
+    print(persisted)
+    cursor = gfs.find({"_id": persisted}).limit(1)
+    res = await cursor.to_list(None)
+    return None if not res else res[0]
 
 async def find_document_by_conversation_id(conversation_id: str):
-    cursor: AsyncIOMotorCursor = document_collection.find({'conversation_id': conversation_id})
+    gfs = AsyncIOMotorGridFSBucket(db)
+    cursor = gfs.find({'metadata.conversation_id': conversation_id})
     return await cursor.to_list(None)
 
 async def exist_by_conversation_id_and_filename(conversation_id: str, filename: str):
-    return document_collection.find_one({'conversation_id': conversation_id, 'filename': filename}) is not None
+    gfs = AsyncIOMotorGridFSBucket(db)
+    cursor = gfs.find({'metadata.conversation_id': conversation_id, 'filename': filename})
+    return await cursor.to_list(None) is not None
 
 async def find_document_by_conversation_id_and_filenames(conversation_id: str, filenames: List[str]) -> List[dict]:
-    cursor: AsyncIOMotorCursor = document_collection.find({'conversation_id': conversation_id, 'filename': { '$in': filenames}})
+    gfs = AsyncIOMotorGridFSBucket(db)
+    cursor = gfs.find({"metadata.conversation_id": conversation_id, "filename": {'$in': filenames}})
     return await cursor.to_list(None)
